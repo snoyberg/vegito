5f74edc5cd10806deb1b5bf35ad547ebae91cf46
Michael Snoyman <michael@snoyman.com>
2016-02-25 20:40:50 +0200

    foldlV

--- src/Vegito.hs
+++ src/Vegito.hs
@@ -44,17 +44,21 @@ enumFromToV low high =
         | otherwise = pure (Done ())
 {-# INLINE enumFromToV #-}
 
-sumV :: (Num i, Monad m) => Source i m () -> m i
-sumV (Source f sorig) =
-    let loop !total s = do
+foldlV :: (Num i, Monad m) => (r -> i -> r) -> r -> Source i m () -> m r
+foldlV g accum0 (Source f sorig) =
+    let loop accum s = do
             step <- f s
             case step of
-                Done () -> pure total
-                Skip s' -> loop total s'
+                Done () -> pure accum
+                Skip s' -> loop accum s'
                 Yield s' i ->
-                    let total' = total + i
-                     in total' `seq` loop total' s'
-     in loop 0 sorig
+                    let accum' = g accum i
+                     in accum' `seq` loop accum' s'
+     in loop accum0 sorig
+{-# INLINE foldlV #-}
+
+sumV :: (Num i, Monad m) => Source i m () -> m i
+sumV = foldlV (+) 0
 {-# INLINE sumV #-}
 
 mapV :: Functor m => (i -> o) -> Source i m r -> Source o m r
